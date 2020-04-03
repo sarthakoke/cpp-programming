6242753bbf30552b060a44a1e59db1bd8b493d9c
sarthakoke <56966186+sarthakoke@users.noreply.github.com>
2020-04-03 22:59:24 +0530

    Add files via upload

---
+++ Question1.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+using namespace std; 
+  
+int main()  
+{  
+	int integersize;  
+	char charactersize;  
+	float floatsize;  
+	double doublesize;  
+
+	cout<< "Size of int is: "<<sizeof(integersize)<<"\n";  
+	cout<< "Size of char is: "<<sizeof(charactersize)<<"\n";
+	cout<< "Size of float is: "<<sizeof(floatsize)<<"\n"; 
+	cout << "Size of double is: "<<sizeof(doublesize)<<"\n";  
+
+	return 0;  
+}
